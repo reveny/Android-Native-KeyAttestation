bddc054b667dff5d9457a4b2fd2c85aed87cce83
reveny <revenyyyy@gmail.com>
2024-02-20 16:42:25 +0100

    Minor code style adjustment

--- app/src/main/jni/Include/Logger.hpp
+++ app/src/main/jni/Include/Logger.hpp
@@ -1,14 +1,10 @@
 //
 // Created by reveny on 26/10/2023.
 //
-
-#ifndef ANDROID_ROOT_DETECTION_LOGGER_H
-#define ANDROID_ROOT_DETECTION_LOGGER_H
+#pragma once
 
 #include <android/log.h>
 
 #define LOG_TAG "KeyAttestation"
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
 #define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__))
-
-#endif
